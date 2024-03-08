4a1037e89723bc44e3a72a14c2d844300720d660
VINCEOFX <vinceofx@gmail.com>
2024-03-08 16:03:03 +0800

    add new

---
+++ __init__.py
@@ -0,0 +1,30 @@
+bl_info = {
+    "name": "Vince Tool",
+    "description": "一个集合多种便捷功能的工具箱",
+    "author": "Vince",
+    "version": (1, 0),
+    "blender": (2, 80, 0),
+    "location": "3D View > Right Pane > Vince Tool",
+    "warning": "",  # used for warning icon and text in addons panel
+    "wiki_url": "http://vinceofx.com/",
+    "category": "3D View",
+}
+
+import bpy
+
+from . import operators
+from . import panels
+
+# 注册面板类和操作类
+def register():
+    operators.register()
+    panels.register()
+
+# 取消注册面板类和操作类
+def unregister():
+    operators.unregister()
+    panels.unregister() 
+    
+# 主程序入口
+if __name__ == "__main__":
+    register()
