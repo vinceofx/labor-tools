6efd2f4815d6222f174e88f0ed9993b193e3dd75
Vince <63284484+VINCEOFX@users.noreply.github.com>
2024-03-12 18:07:30 +0800

    update name

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 bl_info = {
-    "name": "Vince Tool",
+    "name": "Blender Labor",
     "description": "一个集合多种便捷功能的工具箱",
     "author": "Vince",
     "version": (1, 0),
     "blender": (2, 80, 0),
-    "location": "3D View > Right Pane > Vince Tool",
+    "location": "3D View > Right Pane > Blender Labor",
     "warning": "",  # used for warning icon and text in addons panel
     "wiki_url": "http://vinceofx.com/",
     "category": "3D View",
